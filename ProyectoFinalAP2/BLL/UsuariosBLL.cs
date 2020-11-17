d10e7f50a7e057e8211159bb990d887700554ff9
NiurbisReyesJaquez <71194774+NiurbisReyesJaquez@users.noreply.github.com>
2020-11-17 17:26:56 -0400

    BLL Usuario

---
+++ ProyectoFinalAP2/BLL/UsuariosBLL.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAP2.DAL;
+using ProyectoFinalAP2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAP2.BLL
+{
+    public class UsuariosBLL
+    {
+        public static bool Guardar(Usuarios usuario)
+        {
+            if (!Existe(usuario.UsuarioId))
+                return Insertar(usuario);
+            else
+                return Modificar(usuario);
+        }
+
+        public static bool Existe(int id)
+        {
+            bool encontrado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                encontrado = contexto.Usuarios.Any(u => u.UsuarioId == id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+
+        public static bool Insertar(Usuarios usuario)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Usuarios.Add(usuario);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Modificar(Usuarios usuario)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(usuario).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static Usuarios Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Usuarios usuario;
+
+            try
+            {
+                usuario = contexto.Usuarios.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return usuario;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var usuario = contexto.Usuarios.Find(id);
+
+                if (usuario != null)
+                {
+                    contexto.Usuarios.Remove(usuario);
+                    paso = (contexto.SaveChanges() > 0);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static List<Usuarios> GetList(Expression<Func<Usuarios, bool>> criterio)
+        {
+            List<Usuarios> listaUsuarios = new List<Usuarios>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                listaUsuarios = contexto.Usuarios.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return listaUsuarios;
+        }
+
+
+    }
+}
